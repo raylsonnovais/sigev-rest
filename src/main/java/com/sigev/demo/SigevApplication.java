0e6bc9a713c224a5296e14c892c3801d4a0021a4
Raylson Pereira de Novais Silva <raylson.silva@qintess.com.br>
2021-01-06 18:21:26 -0300

    first commit

---
+++ src/main/java/com/sigev/demo/SigevApplication.java
@@ -0,0 +1,13 @@
+package com.sigev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sigev/demo/SigevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
